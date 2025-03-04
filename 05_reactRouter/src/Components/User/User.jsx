a806d9b4d4017052586dc7035f70f745a15070ea
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-03-04 12:05:35 +0500

    done

--- 05_reactRouter/src/Components/User/User.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-const User = () => {
-  const { userId } = useParams();
-  return <div className="bg-gray-600 text-white text-2xl rounded-sm  text-center">user: {userId}</div>;
-};
-
-export default User;
