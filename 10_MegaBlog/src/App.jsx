88f83c84a04302f0930d75efca9b05c915c899d8
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-17 12:28:02 +0500

    setting in appwrite

--- 10_MegaBlog/src/App.jsx
+++ 10_MegaBlog/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL)
+console.log(import.meta.env.VITE_APP_APPWRITE_URL)
   return (
     <>
       <h1>Test</h1>
