4b983e65bac3b85f51d0147b0bdabe216589c6de
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-20 15:34:50 +0500

    update post

--- 10_MegaBlog/src/appwrite/config.js
+++ 10_MegaBlog/src/appwrite/config.js
@@ -32,7 +32,50 @@ export class Service {
     }
   }
 
-  
+  async updatePost(slug, { title, slug, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollection,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: updatePost :: error", error);
+    }
+//   }
+
+//   async deletePost(slug) {
+//     try {
+//       this.databases.deleteDocument(
+//         conf.appwriteDatabaseId,
+//         conf.appwriteCollection,
+//         slug
+//       );
+//       return true;
+//     } catch (error) {
+//       console.log("Appwrite serive :: deletePost :: error", error);
+//       return false;
+//     }
+//   }
+
+//   async getPost(slug) {
+//     try {
+//       return await this.databases.getDocument(
+//         conf.appwriteDatabaseId,
+//         conf.appwriteCollection,
+//         slug
+//       );
+//     } catch (error) {
+//       console.log("Appwrite serive :: getPost :: error", error);
+//       return false;
+//     }
+//   }
 }
 
 const serive = new Service();
