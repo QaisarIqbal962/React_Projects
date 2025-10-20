54bfa1da089c00c2eb17eaa3bad0ade8680ad42b
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-20 15:09:55 +0500

    create post

--- 10_MegaBlog/src/appwrite/config.js
+++ 10_MegaBlog/src/appwrite/config.js
@@ -2,13 +2,37 @@ import conf from "../conf/conf";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
 export class Service {
-  Client = new Client();
+  client = new Client();
   Databases;
   bucket;
-  constructor(){
-     this.client.setEndPoint(conf.endpoint).setProject(conf.projectId);
-        this.account = new Account(this.Client);
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
   }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: createPost :: error", error);
+    }
+  }
+
+  
 }
 
 const serive = new Service();
