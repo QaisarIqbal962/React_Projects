84ae70937c501aee672caddbc494ba830a588b2a
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-29 16:32:59 +0500

    change in CourseForm

--- 11_Zusttut/zusttut/src/App.jsx
+++ 11_Zusttut/zusttut/src/App.jsx
@@ -1,44 +1,20 @@
-import './App.css'
-import courseForm '../components/CourseForm';
-import courseList '../components/CourseList';
+import './App.css';
+import CourseForm from './components/CourseForm';
+import CourseList from './components/CourseList';
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import './App.css';
-// import CourseForm from './components/CourseForm';
-// import CourseList from './components/CourseList';
-
-
-// function App() {
-//   return (
-//     <div className="main-container">
-//       <h1 style={{
-//         fontSize: "2.5rem",
-//         marginBottom: "2rem"
-//       }}> My Course list</h1>
-//       <CourseForm/>
-//       <CourseList/>
+function App() {
+  return (
+    <div className="main-container">
+      <h1 style={{
+        fontSize: "2.5rem",
+        marginBottom: "2rem"
+      }}> My Course list</h1>
+      <CourseForm/>
+      <CourseList/>
       
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
 
-// export default App;
+export default App;
