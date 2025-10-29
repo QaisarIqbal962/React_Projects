bec4f60e58d12bd0e1a178eddca0e89332d87595
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-10-29 16:25:33 +0500

    updated courseStore file

--- 11_Zusttut/zusttut/src/app/courseStore.js
+++ 11_Zusttut/zusttut/src/app/courseStore.js
@@ -1,34 +1,29 @@
-import create from "zustand";
+import create from 'zustand';
 
-import { devtools, persist } from "zustand/middleware";
+import {devtools, persist} from 'zustand/middleware'
 
 const courseStore = (set) => ({
-  courses: [],
-
-  addCourse: (course) => {
-    set((state) => ({
-      courses: [course, ...state.courses],
-    }));
-  },
-
-  removeCourse: (courseId) => {
-    set((state) => ({
-      courses: state.courses.filter((c) => c.id !== courseId),
-    }));
-  },
-
-  toggleCourseStatus: (courseId) => {
-    set((state) => ({
-      courses: state.courses.map((course) =>
-        course.id === completed ? { ...course, completed: !completed } : course
-      ),
-    }));
-  },
+    courses: [],
+    addCourse: (course) => {
+        set((state) => ({
+            courses: [course, ...state.courses],
+        }))
+    },
+    removeCourse: (courseId) => {
+        set((state) => ({
+            courses: state.courses.filter((c) => c.id !== courseId)
+        }))
+    },
+    toggleCourseStatus: (courseId) => {
+        set((state) => ({
+            courses: state.courses.map((course) => course.id === courseId ? {...course, completed: !course.completed} : course)
+        }))
+    }
 })
 
 const useCourseStore = create(
     devtools(
-        persist(courseStore,{
+        persist(courseStore, {
             name: "courses",
         })
     )
