4b16a2878a945060def7567c72746f2b3adaf48a
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-11-06 17:15:31 +0500

    changing ini login file

--- 06_miniContext/src/components/Login.jsx
+++ 06_miniContext/src/components/Login.jsx
@@ -12,19 +12,7 @@ function Login() {
         setUser({username, password})
     }
   return (
-    <div>
-        <h2>Login</h2>
-        <input type='text'
-        value={username}
-        onChange={(e) => setUsername(e.target.value) }
-        placeholder='username'  />
-        {" "}
-        <input type='text' 
-        value={password}
-        onChange={(e) => setPassword(e.target.value) }
-        placeholder='password'  />
-        <button onClick={handleSubmit}>Submit</button>
-    </div>
+    
   )
 }
 
