0341e0d3dbfe68a83c39403a83d270a4bd2ec708
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-11-06 17:14:01 +0500

    changes in user context

--- 12_data/json-data/src/App.jsx
+++ 12_data/json-data/src/App.jsx
@@ -12,7 +12,9 @@ const Getdata = async()=> {
   return (
     <>
       <div className="p-10">
-        
+        <button onClick={Getdata} className="bg-teal-800 text-white font-semibold text-2xl py-6 px-3 rounded border-2 border-gray-500 hover:bg-teal-700 active:scale-90">
+          Get Data
+        </button>
         <div className="p-5 mt-5 bg-gray-950 text-white"></div>
       </div>
     </>
