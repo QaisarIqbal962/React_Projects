33044279bb4a85e9ae5232d1e692e6dbac8cc47f
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-11-07 15:13:32 +0500

    get data

--- 12_data/json-data/src/App.jsx
+++ 12_data/json-data/src/App.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
 import axios from "axios";
-
+import { useState } from "react";
 
 function App() {
-const Getdata = async()=> {
-  const response = await axios.get("https://picsum.photos/v2/list")
-  console.log(response)
- 
 
-}
+ const [data, setData] = useState([])
+  const Getdata = async () => {
+    const response = await axios.get("https://picsum.photos/v2/list");
+    setData(response.data);
+    console.log(data)
+  };
   return (
     <>
       <div className="p-10">
-        <button onClick={Getdata} className="bg-teal-800 text-white font-semibold text-2xl py-6 px-3 rounded border-2 border-gray-500 hover:bg-teal-700 active:scale-90">
+        <button
+          onClick={Getdata}
+          className="bg-teal-800 text-white font-semibold text-2xl py-6 px-3 rounded border-2 border-gray-500 hover:bg-teal-700 active:scale-90"
+        >
           Get Data
         </button>
-        <div className="p-5 mt-5 bg-gray-950 text-white"></div>
+        <div className="p-5 mt-5 bg-gray-950 text-white">
+          {data.map((elem,idx)=> {
+            return  <div className="bg-gray-50 text-black flex"></div>
+           
+
+
+          })}
+        </div>
       </div>
     </>
   );
