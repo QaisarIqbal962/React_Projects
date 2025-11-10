d8bb3c3249e5c3cac019d05fefd64c1d3680085b
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-11-10 22:46:01 +0500

    change the button

--- 04_currency-converter/src/Components/Converter.jsx
+++ 04_currency-converter/src/Components/Converter.jsx
@@ -54,22 +54,7 @@ const Converter = () => {
     setAmountConverted(0);
   };
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
-      <h1 className="text-4xl font-extrabold text-white mb-6 shadow-lg">
-        Currency Converter
-      </h1>
-      <div className="bg-white shadow-xl rounded-lg p-8 w-full max-w-md transition-transform transform hover:scale-105">
-        <div className="mb-5">
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            className="border border-gray-300 p-4 rounded-lg w-full focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-300 transition duration-300"
-            placeholder={`Enter amount in ${selectedCurrency}`}
-            min="0"
-          />
-        </div>
+
 
         <div className="flex items-center mb-5">
           <select
