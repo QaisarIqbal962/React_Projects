967078911b6bcd748e220950864b796218afe8bf
Qaisar iqbal <qaisarbangash47@gmail.com>
2026-03-10 14:20:55 +0500

    add

---
+++ 06_miniContext/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<h1 class="text-3xl font-bold  bg-purple-400 text-white rounded-tr-lg rounded-bl-lg p-4 font-semibold">
+    Hello world!
+  </h1>    </>
+  )
+}
+
+export default App
